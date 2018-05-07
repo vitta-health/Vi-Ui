e6e33eacf67a47dd54f3515e01526321a5f489bf
ricardo_alvarenga <ricardo.alvarenga@vitta.me>
2018-05-07 10:38:19 -0300

    feat: adicionado componente progress a biblioteca de componentes

---
+++ src/components/Progress/Progress.vue
@@ -0,0 +1,108 @@
+<template>
+  <div
+    ref="progress"
+    :class="[percentValueComputed ? 'progress-dynamic': 'progress-loop']"
+    :style="[percentValueComputed? { height: size+'px', width: size+'px', background: 'linear-gradient(#3581BC ' + percentValueComputed + '%, #f3f3f3 0%)' }: { height: size+'px', width: size+'px'}]">
+    <div
+      ref="content"
+      class="content"
+      :style="{ height: borderSizeComputed+'px', width: borderSizeComputed+'px' }">
+      <span v-if="percentValueComputed" :style="{ 'font-size': size / 2.8+'px' }">{{ percentValueComputed }}%</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import loggerMixin from "../../mixins/loggerMixin";
+import { setTimeout, clearTimeout } from 'timers';
+
+/**
+ * Barra de progresso circular.
+ */
+export default {
+  name: "Progress",
+  mixins: [loggerMixin],
+  props: {
+    /**
+     * Define o tamanho do progress
+     */
+    size: {
+      type: [Number, String],
+      default: 32,
+    },
+    /**
+     * Define o tamanho da borda
+     */
+    borderSize: {
+      type: [Number, String],
+      default: 4,
+		},
+		/**
+		 * Define o percentual do carregamento (Sem valor definido o circulo apenas gira infinitamente)
+		 */
+		percentValue: {
+			type: [Number, String],
+			default: null,
+		}
+  },
+  data () {
+    return {
+      loading: 0,
+    }
+  },
+  computed: {
+    borderSizeComputed() {
+      return this.borderSize >= this.size ? 1 : (this.size - this.borderSize);
+    },
+    percentValueComputed() {
+      return this.percentValue === null ? 0 : this.percentValue;
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+.content {
+  background-color: white;
+  border-radius: 50%;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.progress-loop {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border-radius: 50%;
+  background: linear-gradient(#3581BC 30%, #f3f3f3 40%);
+  -webkit-animation: spin 1s linear infinite; /* Safari */
+  animation: spin 1s linear infinite;
+}
+
+.progress-dynamic {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  border-radius: 50%;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+</style>
+
---
+++ src/components/Progress/Readme.md
@@ -0,0 +1,31 @@
+Progress circular em loop:
+
+```jsx
+<Progress />
+``` 
+
+Progress circular dinamico:
+
+```jsx
+<Progress percentValue="25" />
+``` 
+
+Veja como é facil utilizar
+
+```vue
+<template>
+    <div class="wrapper">
+        <Progress size="64" borderSize="12" />
+    </div>
+</template>
+<script>
+
+export default {
+    methods: {
+        // your code here
+    }
+}
+</script>
+<style scoped>
+</style>
+```
