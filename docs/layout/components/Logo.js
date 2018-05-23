2686b802053c6356fc82ff0fa4a14a876bae99a9
Jean Souza <jean.co.souza@gmail.com>
2018-05-23 10:01:48 -0300

    feat: style theme, icons e texto

--- docs/layout/components/Logo.js
+++ docs/layout/components/Logo.js
@@ -1,32 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Styled from 'rsg-components/Styled';
-import logo from './vittaninja.png';
+import logo from './vi-ui.svg';
+
+const npmPac = require('../../../package.json');
 
 const styles = ({ fontFamily }) => ({
   logo: {
-    textAlign: 'center',
-    margin: 0,
+    margin: '0 0 10px',
     fontFamily: fontFamily.base,
     fontSize: 18,
-    fontWeight: 'normal',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   image: {
-    width: '55%',
-    margin: '15px auto',
+    height: '118px',
   },
   title: {
-    fontSize: 12,
-    textAlign: 'center',
-    color: '#fff',
-  }
+    color: '#333',
+    fontSize: 63,
+    marginLeft: '10px',
+  },
+  version: {
+    display: 'block',
+    fontWeight: '700',
+    textAlign: 'right',
+    fontSize: 15,
+    color: '#4ac7ff',
+  },
 });
 
 export function LogoRenderer({ classes, children }) {
   return (
     <div className={classes.logo}>
+      <img className={classes.image} src={logo} alt="Vitta Ui" />
+      <div className={classes.title} title={children}>
+        Vi-Ui
+        <small className={classes.version}>v{npmPac.version}</small>
+      </div>
     </div>
-
   );
 }
 
--- docs/styleguide.config.js
+++ docs/styleguide.config.js
@@ -1,31 +1,42 @@
 const path = require('path');
 const webpackConfig = require('../webpack.config');
-const sectionsMenu = require('./docs/sections');
+const sectionsMenu = require('./pages/sections');
 
 module.exports = {
-  title: 'Vitta Styleguide',
+  title: 'Vi-Ui Documentation',
   components: '../src/components/[A-Z]*.vue',
   sortProps: props => props,
   webpackConfig,
   showUsage: true,
   showCode: true,
   require: ['babel-polyfill', path.join(__dirname, './layout/style.css')],
+  styleguideComponents: {
+    SectionRenderer: path.join(__dirname, 'layout/components/Section'),
+    HeadingRenderer: path.join(__dirname, 'layout/components/Heading'),
+    SectionHeadingRenderer: path.join(__dirname, 'layout/components/SectionHeading'),
+    LogoRenderer: path.join(__dirname, 'layout/components/Logo'),
+    StyleGuideRenderer: path.join(__dirname, 'layout/components/StyleGuide'),
+    LinkRenderer: path.join(__dirname, 'layout/components/Link'),
+    ComponentsListRenderer: path.join(__dirname, 'layout/components/ComponentsList'),
+    TableOfContentsRenderer: path.join(__dirname, 'layout/components/TableOfContents'),
+  },
   theme: {
     color: {
       base: '#333',
-      sidebarBackground: '#28588C',
+      sidebarBackground: '#fff',
       link: '#0E96D3',
       linkHover: '#28588C',
       error: '#E45B5B',
     },
+    sidebarWidth: 300,
     fontWeigth: 300,
     fontSize: {
       base: 16,
       text: 16,
       small: 13,
-      h1: 48,
-      h2: 36,
-      h3: 24,
+      h1: 29,
+      h2: 25,
+      h3: 20,
       h4: 18,
       h5: 16,
       h6: 16,
@@ -34,6 +45,11 @@ module.exports = {
       base: ['"Roboto"', '"RobotoFallback"', '"Helvetica Neue"', 'sans-serif'],
       monospace: ['Consolas', '"Liberation Mono"', 'Menlo', 'monospace'],
 
+    },
+  },
+  styles: {
+    Link: {
+      color: '#333',
     },
   },
   sections: sectionsMenu,
