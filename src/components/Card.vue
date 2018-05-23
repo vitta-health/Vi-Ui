46da549ce424b05d21c5d990a5bb0affcb675d0e
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2018-05-23 19:00:25 -0300

    fix: Ajuste stilo card exemplo

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -48,75 +48,64 @@ export default {
 Card Básico
 ```vue
 <template>
-<div class="greyBox">
-<vi-card :width="300" class="ViCard-Element">
-  <h1 slot="Header" class="title-example">Desaparecido</h1>
-  <div slot="Body">
-    <img class="avatar" src="https://i.imgur.com/LuyN22o.jpg?1"/>
-    <div class="descrition">
-      <h4 class="name">Peter Parker aka Spiderman</h4>
-      <span class="birth-day">08/01/1989 (29a e 7m)</span>
-      <span>Masculino</span>
-    </div>
+  <div class="greyBox">
+    <vi-card v-for="(character, index) in characters" width="300" class="ViCard-Element">
+      <h1 slot="Header" class="title-example textTitle">Desaparecido</h1>
+      <div slot="Body">
+        <img class="avatar" :src="character.avatar"/>
+        <div class="descrition">
+          <h4 class="name">{{character.name}} aka {{character.superHeroName}}</h4>
+          <span class="birth-day">{{character.birthDate}} {{character.age}}</span>
+          <span>{{character.gender}}</span>
+        </div>
+      </div>
+      <div slot="Footer">
+        <vi-button success large width="100%">Avançar!</vi-button>
+      </div>
+    </vi-card>
   </div>
-  <div slot="Footer">
-    <vi-button :large="true" :width="'116%'" class="button">Avançar!</vi-button>
-  </div>
-</vi-card>
-<vi-card :width="300">
-  <h1 slot="Header" class="title-example">Desaparecido</h1>
-  <div slot="Body">
-    <img class="avatar" src="https://i.imgur.com/g6nGMDv.jpg"/>
-    <div class="descrition">
-      <h4 class="name">James Howlett logan aka Wolverine</h4>
-      <span class="birth-day">??/??/1880 (127a)</span>
-      <span>Masculino</span>
-    </div>
-  </div>
-  <div slot="Footer">
-    <vi-button :large="true" :width="'116%'" class="button">Avançar!</vi-button>
-  </div>
-</vi-card>
-
-<vi-card :width="300">
-  <h1 slot="Header" class="title-example">Desaparecido</h1>
-  <div slot="Body">
-    <img class="avatar" src="https://i.imgur.com/g6nGMDv.jpg"/>
-    <div class="descrition">
-      <h4 class="name">James Howlett logan aka Wolverine</h4>
-      <span class="birth-day">??/??/1880 (127a)</span>
-      <span>Masculino</span>
-    </div>
-  </div>
-  <div slot="Footer">
-    <vi-button :large="true" :width="'116%'" class="button">Avançar!</vi-button>
-  </div>
-</vi-card>
-
-<vi-card :width="300">
-  <h1 slot="Header" class="title-example">Desaparecido</h1>
-  <div slot="Body">
-    <img class="avatar" src="https://i.imgur.com/g6nGMDv.jpg"/>
-    <div class="descrition">
-      <h4 class="name">James Howlett logan aka Wolverine</h4>
-      <span class="birth-day">??/??/1880 (127a)</span>
-      <span>Masculino</span>
-    </div>
-  </div>
-  <div slot="Footer">
-    <vi-button large :width="'116%'" class="button">Avançar!</vi-button>
-  </div>
-</vi-card>
-
-</div>
 </template>
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      characters: [
+        {
+          name: 'Peter Parker',
+          superHeroName: 'Spider-Man',
+          birthDate: '08/01/1989',
+          age: '(29a e 7m)',
+          avatar: 'https://i.imgur.com/LuyN22o.jpg?1',
+          gender: 'Masculino'
+        },
+        {
+          name: 'James Howlett logan',
+          superHeroName: 'Wolverine',
+          birthDate: '??/??/1880',
+          age: '(128a)',
+          avatar: 'https://i.imgur.com/g6nGMDv.jpg',
+          gender: 'Masculino',
+        },
+        {
+          name: 'Steven "Steve" Rogers',
+          superHeroName: 'Capitão América',
+          birthDate: '??/??/1941',
+          age: '(128a)',
+          avatar: 'https://i.imgur.com/S6Jfkqa.jpg',
+          gender: 'Masculino',
+        },
+      ],
+    }
+  }
+}
 </script>
 <style>
+  h1 {
+    font-size: 1.4em;
+  }
   .title-example {
     margin-top: 0;
-    text-align: center;
+    text-align: left;
   }
   .greyBox{
     background: #EAEAEA;
@@ -138,14 +127,29 @@ export default {}
   .birth-day {
     white-space: nowrap;
   }
-  .button {
-    top: 21px;
+  .ViCard {
     position: relative;
-    right: 20px;
+    padding: 38px 20px 63px;
+    line-height: 1em;
+    overflow: hidden;
   }
-
-  .ViCard-Element {
-    margin-bottom: 20px;
+  .ViButton {
+    border-radius: 0.2em;
+    bottom: 0;
+    position: absolute;
+    left: 0;
+  }
+  .textTitle {
+    background-color: #798F98;
+    height: 40px;
+    line-height: 40px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    color: #fff;
+    padding-left: 21px;
+    font-weight: 400;
   }
 </style>
 ```
