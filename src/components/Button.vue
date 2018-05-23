c84fa2430d723e0ce259b72b6092fb9cd6f926bb
ricardo_alvarenga <ricardo.alvarenga@vitta.me>
2018-05-23 11:55:10 -0300

    wip: ajustte no retorno da url e também nos botoes de navegação

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,34 @@
 <template>
+  <a
+    v-if="href"
+    ref="button"
+    class="ViButton"
+    :class="[
+      {
+        'ViButton--mini': mini,
+        'ViButton--small': small,
+        'ViButton--large': large,
+        'ViButton--pill': pill,
+        'ViButton--active': active,
+      },
+      colorClass({
+        background: !outlined,
+        border: outlined,
+        hover: true && !active
+      }),
+    ]"
+    :href="this.href"
+    :style="{
+      width: buttonWidth,
+      justifyContent,
+    }"
+    @click="onClick">
+    <vi-wrapper
+      :justify-content="justifyContent"
+      spacing="10"><slot /></vi-wrapper>
+  </a>
   <button
+    v-else
     ref="button"
     class="ViButton"
     :class="[
@@ -11,7 +40,6 @@
       },
       colorClass({ background: !outlined, border: outlined, hover: true }),
     ]"
-    :href="this.href"
     :style="{
       width: buttonWidth,
       justifyContent,
@@ -40,6 +68,13 @@ export default {
      * Fundo transparente com apenas borda e texto colorido
      */
     outlined: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Define se o botão esta selecionado
+     */
+    active: {
       type: Boolean,
       default: false,
     },
@@ -79,13 +114,6 @@ export default {
     },
   },
   methods: {
-    changeTag() {
-      const tagName = this.$refs.button.outerHTML;
-      const regex = /^<button/;
-      if (this.href || this.tag) {
-        this.$refs.button.outerHTML = `<${tagName.replace(regex, this.tag || 'a')}`;
-      }
-    },
     onClick() {
       /**
        * Evento de clique.
@@ -95,9 +123,6 @@ export default {
        */
       this.$emit('on-click');
     },
-  },
-  mounted() {
-    this.changeTag();
   },
 };
 </script>
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -4,31 +4,34 @@
     outlined>
     <vi-button
       title="Primeira Página"
-      @on-click="goto('FIRST_PAGE')"
+      @on-click="handleNavitation('FIRST_PAGE')"
+      :href="getURL('FIRST_PAGE')"
       primary
       :disabled="!buttonEnabled('FIRST_PAGE')">◂◂</vi-button>
     <vi-button
-      @on-click="goto('PREVIOUS_PAGE')"
+      @on-click="handleNavitation('PREVIOUS_PAGE')"
       primary
+      :href="getURL('PREVIOUS_PAGE')"
       :disabled="!buttonEnabled('PREVIOUS_PAGE')">◂</vi-button>
     <vi-button
       primary
-      @on-click="goto(index)"
-      :disabled="isCurrentPage(index)"
-      :selected="isCurrentPage(index)"
-      v-for="(page, index) in updateTotalShowed"
-      :key="index"
-      v-if="index >= (updateTotalShowed - totalShowed)">{{ page }}
+      :href="getURL(page)"
+      @on-click="handleNavitation(page, true)"
+      :active="isCurrentPage(page)"
+      v-for="(page) in getPages"
+      :key="page">{{ page }}
     </vi-button>
     <vi-button
-      @on-click="goto('NEXT_PAGE')"
+      @on-click="handleNavitation('NEXT_PAGE')"
       primary
+      :href="getURL('NEXT_PAGE')"
       :disabled="!buttonEnabled('NEXT_PAGE')">▸</vi-button>
     <vi-button
       primary
       title="Última Página"
-      @on-click="goto('LAST_PAGE')"
-      :disabled="buttonEnabled('LAST_PAGE')">▸▸</vi-button>
+      @on-click="handleNavitation('LAST_PAGE')"
+      :href="getURL('LAST_PAGE')"
+      :disabled="!buttonEnabled('LAST_PAGE')">▸▸</vi-button>
   </vi-button-group>
 </template>
 
@@ -72,15 +75,9 @@ export default {
     },
   },
   methods: {
-    /**
-      * Evento de disparado ao clicar nos botões de navegação da paginação.
-      *
-      * @event onAtualizaPagina
-      * @type {object}
-      */
-    goto(newPage) {
+    getPage(newPage) {
       let goToPage = newPage;
-      const countPages = this.totalPages ? this.totalPages : this.updateTotalShowed;
+      const countPages = this.totalPages ? this.totalPages : this.getPagesRange[1];
       if (newPage === 'PREVIOUS_PAGE') {
         goToPage = this.currentPage <= 1 ? 1 : this.currentPage - 1;
       } else if (newPage === 'NEXT_PAGE') {
@@ -89,31 +86,39 @@ export default {
         goToPage = 1;
       } else if (newPage === 'LAST_PAGE') {
         goToPage = countPages;
-      } else {
-        goToPage = newPage + 1;
       }
-      const baseUrl = this.baseURL ? `${this.baseURL}?page=${goToPage}` : null;
-      this.$emit('on-atualiza-pagina', { currentPage: goToPage, baseUrl });
+      return goToPage;
+    },
+    getURL(page) {
+      return this.baseURL ? this.baseURL.replace('##PAGE##', this.getPage(page)) : null;
+    },
+    /**
+     * Evento disparado ao clicar na navegação
+     * @event onChangePage
+     * @type {object}
+     */
+    handleNavitation(page) {
+      this.$emit('on-change-page', { page: this.getPage(page) });
     },
     buttonEnabled(button) {
       if (button === 'LAST_PAGE') {
-        return this.currentPage >= this.updateTotalShowed || this.totalPages === 0;
+        return this.totalPages > 0 && this.currentPage < this.totalPages;
       } else if (button === 'NEXT_PAGE') {
-        return this.currentPage < this.updateTotalShowed;
+        return this.currentPage < this.totalPages || this.totalPages === 0;
       } else if (button === 'FIRST_PAGE') {
-        return this.currentPage <= this.updateTotalShowed && this.currentPage > 1;
+        return this.currentPage > 1;
       }
       return this.currentPage > 1;
     },
-    isCurrentPage(index) {
-      return this.currentPage === (index + 1);
+    isCurrentPage(page) {
+      return this.currentPage === page;
     },
   },
   computed: {
-    updateTotalShowed() {
+    getPagesRange() {
       const middle = Math.ceil(this.totalShowed / 2);
       if (this.currentPage < middle) {
-        return this.totalShowed;
+        return [1, this.totalShowed];
       }
       let countPages = 0;
       const diff = this.currentPage - middle;
@@ -124,7 +129,15 @@ export default {
         first -= (last - countPages);
         last -= (last - countPages);
       }
-      return last;
+      return [first, last];
+    },
+    getPages() {
+      const [first, last] = this.getPagesRange;
+      const pages = [];
+      for (let i = first; i <= last; i += 1) {
+        pages.push(i);
+      }
+      return pages;
     },
 
   },
@@ -146,24 +159,24 @@ export default {
   <vi-pagination
     :currentPage="currentPage"
     :baseURL="baseURL"
-    :totalPages="totalPages"
+
     :totalShowed="totalShowed"
-    @on-atualiza-pagina="page => mudaPagina(page)"/>
+    @on-change-page="page => mudaPagina(page)"/>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      currentPage: 4,
-      baseURL:'baseurl.com.br',
+      currentPage: 3,
+      baseURL:'/#pagination?page=##PAGE##',
       totalPages: 10,
       totalShowed: 5
     };
   },
   methods: {
     mudaPagina(page) {
-      this.currentPage = page.currentPage;
+      this.currentPage = page.page;
     }
   }
 };
