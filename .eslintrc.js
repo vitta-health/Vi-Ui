391ce036398d89839a04516ee74fab1ec79e1909
Jean Souza <713484+jeancarlos@users.noreply.github.com>
2019-09-18 10:11:29 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,10 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es6: true,
   },
   extends: ['plugin:vue/essential', '@vue/prettier'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
   parserOptions: {
     parser: 'babel-eslint',
   },
