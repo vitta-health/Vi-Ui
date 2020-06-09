9684473df6ae38b86c933f14d8a4968003a13235
Luiz Fernando Rodrigues <luiiz_roodrigues@hotmail.com>
2020-06-09 17:44:33 -0300

    fixing generated bundle, minor improve on deploy

--- src/index.js
+++ src/index.js
@@ -5,19 +5,16 @@ const ViUi = {
    *
    * Method responsible for register Vi-Ui on VueJS
    * @param Vue -> Vue install for Vi-Ui register
-   * @param Options -> Additional configuration that can be use on Vi-ui register
    *  Eg.: A La-carte components and theme configuration with JS.
    */
-  install(Vue, Options) {
-
-
+  install(Vue) {
     // Get all Vi-Ui components and install them.
     Object.keys(components).forEach(component => {
-      let componentName = components[component].name;
+      const componentName = components[component].name;
       // Registering the component.
       // Here is a interesting place to do dinamic register. Register a component only if it was specified on Options.
       Vue.component(componentName, components[component]);
-    })
+    });
 
     // Below can be register Vue.directive, Vue.filter and etc, following the above code.
 
